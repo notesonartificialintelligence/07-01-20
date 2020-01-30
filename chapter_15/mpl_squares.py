07cf62e7e194791417ca2f1dbf393c9ca3abe9b9
notesonartificialintelligence <59626140+notesonartificialintelligence@users.noreply.github.com>
2020-01-30 23:25:21 +0000

    Start of Chapter 15
    Data Visualisation

---
+++ chapter_15/mpl_squares.py
@@ -0,0 +1,12 @@
+#Gabriel Abraham
+#notesonartificialintelligence
+#Python Crash Course - Project 1 - Chapter 15
+
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+
+fig, ax = plt.subplots()
+ax.plot(squares)
+
+plt.show()
