614bef16b778588778b19b82d724695f43c554ce
notesonartificialintelligence <59626140+notesonartificialintelligence@users.noreply.github.com>
2020-01-31 23:03:40 +0000

    Start of Chapter 12

--- chapter_12/alien_invasion.py
+++ chapter_12/alien_invasion.py
@@ -6,15 +6,21 @@ import sys
 
 import pygame
 
+from setting import Settings
+
 class AlienInvasion:
 	"""Overall class to manage game assets and behavior."""
 
 	def __init__(self):
 		"""Initalise the game, and create game resources."""
 		pygame.init()
+		self.settings = Settings()
 
-		self.screen = pygame.display.set_mode((1200,800))
+		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
+
+		#Set the background color.
+		self.bg_color = (230, 230,230)
 
 	def run_game(self):
 		"""Start the main loop for the game."""
@@ -23,6 +29,9 @@ class AlienInvasion:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					sys.exit()
+
+			#Redraw the screen during each pass through the loop.
+			self.screen.fill(self.setting.bg_color)
 
 			#Make the most revently drawn screen visible.
 			pygame.display.flip()
---
+++ chapter_12/settings.py
@@ -0,0 +1,13 @@
+#Gabriel Abraham
+#notesonartificialintelligence
+#Python Crash Course - Project 1 - Chapter 12
+
+class Settings:
+	"""A class to store al settings for Alien Invasion."""
+
+	def __init__():
+		"""Initalise the game's settings."""
+		#Screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.by_color = (230,230,230)
